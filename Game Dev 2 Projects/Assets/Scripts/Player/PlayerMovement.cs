2831ff7b3637b59847aee0127c3fa18ce69d87fd
elbowGeese <eapricemail@gmail.com>
2025-10-21 18:42:23 -0700

    Player Move and Camera Input

---
+++ Game Dev 2 Projects/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // VARIABLES
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 30f;
+
+    // REFERENCES
+    private InputAction moveAction;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        // MOVE
+        Vector2 inputDir = moveAction.ReadValue<Vector2>();
+        Transform cameraHandle = Camera.main.transform;
+
+        Vector3 worldForward = cameraHandle.forward;
+        worldForward.y = 0f;
+        worldForward.Normalize();
+
+        Vector3 worldRight = cameraHandle.right;
+        worldRight.y = 0f;
+        worldRight.Normalize();
+
+        Vector3 moveDir = Vector3.zero;
+        moveDir += worldRight * inputDir.x;
+        moveDir += worldForward * inputDir.y;
+
+        if (moveDir.magnitude > 1f) { moveDir.Normalize(); }
+
+        navMeshAgent.Move(moveDir * moveSpeed * Time.deltaTime);
+
+        // ROTATE
+        Vector3 targetLook = Vector3.Lerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+        transform.LookAt(targetLook + transform.position);
+    }
+}
